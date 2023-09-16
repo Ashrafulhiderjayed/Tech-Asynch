f3bfdd8b31010d7c0733a57612edeeae14048c6d
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-09-16 18:42:42 +0600

    make responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import placeholderimg from '../src/assets/placeholderimg.jpg'
+import placeholderimg from "../src/assets/placeholderimg.jpg";
 // import {placeholderimg} from '../src/assets/placeholderimg.jpg'
 
 function App() {
   return (
     <>
-      <section className="ms-10">
-        <div className="">
+      <section className="lg:ms-10">
+        <div>
           <svg
             className="absolute top-0 right-0"
             viewBox="0 120 70 14"
@@ -20,29 +20,43 @@ function App() {
           </svg>
         </div>
 
-        <div className="max-w-7xl border">
-          <h1 className="text-primaryOrange text-3xl font-extrabold pt-7">TechAsync</h1>
+        <div className="max-w-5xl">
+          <h1 className="text-primaryOrange text-3xl font-extrabold pt-7 sm:ms-2">
+            TechAsync
+          </h1>
         </div>
 
-        <div className="grid grid-cols-5 gap-4 mt-24">
-          {/* <!-- First Div (Takes 2 columns) --> */}
-          <div className="flex items-center col-span-2 p-4">
-            <div>
-            <h1 className="text-slate-500 font-semibold">WEBSITE</h1> <br />
-            <h1 className="text-primaryOrange font-bold">COMING SOON!</h1> <br />
-            <p className="text-justify">Explore our world of software excellence, where user-friendly interfaces, seamless integration, and robust security are our hallmarks. Join us on a journey to harness the full potential of technology and unlock new possibilities for your success.</p> <br />
-            <button className="btn btn-secondary bg-primaryOrange">More info</button>
+        
+        <div className="flex flex-wrap mt-24">
+         {/* Second Div (Entire width on small screens, 3 columns on larger screens) */}
+          <div className="w-full flex-col items-center lg:w-2/5 sm:w-3/5 p-4 order-last sm:order-first">
+            <div className="p-4 mt-16">
+              <h1 className="text-slate-500 font-semibold">WEBSITE</h1> <br />
+              <h1 className="text-primaryOrange text-2xl font-bold">
+                COMING SOON!
+              </h1>{" "}
+              <br />
+              <p className="text-justify">
+              Our team of brilliant minds, seasoned experts, and creative visionaries work tirelessly to design and develop software that solves complex problems, enhances productivity, and drives growth. Whether you're a startup with big dreams or an established enterprise seeking to stay ahead of the curve, we've got the technology solutions to propel you forward.
+
+Explore our world of software excellence, where user-friendly interfaces, seamless integration, and robust security are our hallmarks. Join us on a journey to harness the full potential of technology and unlock new possibilities for your success.
+              </p>
+              <br />
+              <button className="btn btn-secondary bg-primaryOrange">
+                More info
+              </button>
             </div>
           </div>
-          
-          {/* <!-- Second Div (Takes 3 columns) --> */}
-          <div className="col-span-3">
+
+           {/* First Div (Entire width on small screens, 2 columns on larger screens) */}
+  <div className="w-full lg:w-3/5 sm:w-2/5 ps-2 order-first sm:order-last">
             <img src={placeholderimg} alt="placeholder image" />
           </div>
         </div>
-      
+
+        
       </section>
-      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FA4D56" fill-opacity="1" d="M0,160L48,170.7C96,181,192,203,288,229.3C384,256,480,288,576,282.7C672,277,768,235,864,181.3C960,128,1056,64,1152,74.7C1248,85,1344,171,1392,213.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> */}
+      
     </>
   );
 }
